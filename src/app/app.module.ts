f90529f2f7aaf9ae739a500bf61dd3292ce6c761
dymafr <erwan1309@hotmail.com>
2020-12-28 13:16:57 +0100

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { CocktailListComponent } from "./cocktail-container/cocktail-list/cockta
 import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
 import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
 import { SelectedDirective } from "./shared/directives/selected.directive";
-import { PanierContainerComponent } from './panier-container/panier-container.component';
-import { IngredientListComponent } from './panier-container/ingredient-list/ingredient-list.component';
+import { PanierContainerComponent } from "./panier-container/panier-container.component";
+import { IngredientListComponent } from "./panier-container/ingredient-list/ingredient-list.component";
+import { RouterModule } from "@angular/router";
+import { APP_ROUTES } from "./app.routes";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(APP_ROUTES)],
   declarations: [
     AppComponent,
     HeaderComponent,
